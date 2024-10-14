1efb22268ef43c2c67c94e0ff5b1a0e8deb31cb4
Aya Elagamy <161774628+elagamy257@users.noreply.github.com>
2024-10-14 21:47:11 +0300

    Add files via upload

---
+++ 01-speing-boot-overview/03-dev-security-demo/MyApp/src/test/java/com/elagamy/MyApp/MyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elagamy.MyApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
