539c43b638a569d719047e8393ca4cb137c96fd2
Aya Elagamy <161774628+elagamy257@users.noreply.github.com>
2024-10-15 15:42:38 +0300

    Add files via upload

---
+++ 02-spring-boot-core/05-primary/src/main/java/com/mycode/springdemo/common/TrackCoach.java
@@ -0,0 +1,15 @@
+package com.mycode.springdemo.common;
+
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class TrackCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "TrackCoach";
+    }
+}
