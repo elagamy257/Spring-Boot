8bebc1e1406b928ec05adaa26a8f6ae8e9739f7b
Aya Elagamy <161774628+elagamy257@users.noreply.github.com>
2024-10-16 21:55:36 +0300

    Add files via upload

---
+++ 02-spring-boot-core/06-lazy-init/src/main/java/com/mycode/springdemo/common/BaseballCoach.java
@@ -0,0 +1,17 @@
+package com.mycode.springdemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach {
+
+    public BaseballCoach() {
+        System.out.println("In constructor: "+getClass().getSimpleName());
+    }
+
+
+    @Override
+    public String getDailyWorkout() {
+        return "BaseballCoach :)";
+    }
+}
---
+++ 02-spring-boot-core/06-lazy-init/src/main/java/com/mycode/springdemo/rest/demoCon.java
@@ -0,0 +1,24 @@
+package com.mycode.springdemo.rest;
+
+import com.mycode.springdemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class demoCon {
+    private Coach coach;
+
+    @Autowired
+    public void DemoCon(@Qualifier("cricketCoach") Coach theCoach) {
+        System.out.println("In constructor: "+getClass().getSimpleName());
+        coach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return coach.getDailyWorkout();
+    }
+
+}
