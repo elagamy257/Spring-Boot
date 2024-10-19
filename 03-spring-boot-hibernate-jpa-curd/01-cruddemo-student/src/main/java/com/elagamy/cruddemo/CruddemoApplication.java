f076ade5bd38ddd4bfc7d13a8fbadbf4d79ecdaa
Aya Elagamy <161774628+elagamy257@users.noreply.github.com>
2024-10-19 12:54:44 +0300

    Update CruddemoApplication.java

--- 03-spring-boot-hibernate-jpa-curd/01-cruddemo-student/src/main/java/com/elagamy/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-curd/01-cruddemo-student/src/main/java/com/elagamy/cruddemo/CruddemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -17,9 +19,33 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner ->{
-			createStudent(studentDAO);
-			createMultipleStudents(studentDAO);
+			//createStudent(studentDAO);
+			//createMultipleStudents(studentDAO);
+			//readStudent(studentDAO);
+			//queryForStudents(studentDAO);
+			queryForStudentsByLastName(studentDAO);
+
 		};
+	}
+
+	private void queryForStudentsByLastName(StudentDAO studentDAO) {
+		// get a list of students
+		List<Student> theStudents = studentDAO.findByLastName("sallam");
+
+		// display list of students
+		for (Student tempStudent : theStudents) {
+			System.out.println(tempStudent);
+		}
+	}
+
+	private void queryForStudents(StudentDAO studentDAO) {
+		// get a list of students
+		List<Student> theStudents = studentDAO.findAll();
+
+		// display list of students
+		for (Student tempStudent : theStudents) {
+			System.out.println(tempStudent);
+		}
 	}
 
 	private void createMultipleStudents(StudentDAO studentDAO) {
@@ -49,6 +75,28 @@ public class CruddemoApplication {
 
 		// display id of the saved student
 		System.out.println("Saved student. Generated id: " + tempStudent.getId());
+	}
+
+	private void readStudent(StudentDAO studentDAO) {
+
+		// create  a student object
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("radw", "adel", "radw@elagamy.com");
+
+		// save the student
+		System.out.println("Saving the student ...");
+		studentDAO.save(tempStudent);
+
+		// display id of the saved student
+		int theId = tempStudent.getId();
+		System.out.println("Saved student. Generated id: " + theId);
+
+		// retrieve student based on the id: primary key
+		System.out.println("Retrieving student with id: " + theId);
+		Student myStudent = studentDAO.findById(theId);
+
+		// display student
+		System.out.println("Found the student: " + myStudent);
 	}
 }
 
