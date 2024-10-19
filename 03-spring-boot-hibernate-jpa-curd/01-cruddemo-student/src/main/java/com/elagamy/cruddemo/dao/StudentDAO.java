4e8a74121dd56b8590ea7333734cf5f9c5fb3e4d
Aya Elagamy <161774628+elagamy257@users.noreply.github.com>
2024-10-19 12:55:30 +0300

    Update StudentDAO.java

--- 03-spring-boot-hibernate-jpa-curd/01-cruddemo-student/src/main/java/com/elagamy/cruddemo/dao/StudentDAO.java
+++ 03-spring-boot-hibernate-jpa-curd/01-cruddemo-student/src/main/java/com/elagamy/cruddemo/dao/StudentDAO.java
@@ -3,7 +3,15 @@ package com.elagamy.cruddemo.dao;
 import com.elagamy.cruddemo.entity.Student;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 public interface StudentDAO {
     @Transactional
     void save(Student theStudent);
+
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String theLastName);
 }
